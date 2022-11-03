4c5dd958fe4aba810339733c0fc05db869333a9c
Sharad <ayushpal5432@gmail.com>
2022-11-03 22:15:54 +0530

    Update Sql.java

--- Sql.java
+++ Sql.java
@@ -1,7 +1,5 @@
 package JDBC;
 
-import java.util.Scanner;
-
 public class Sql {
 
     public static void main(String[] args) throws Exception {
@@ -11,11 +9,11 @@ public class Sql {
 
         for(int a = 1;;a++){
 
-            Thread.sleep(10);
+            Thread.sleep(1000);
             System.out.println(h1 + "" + h2+":"+m1+""+m2+":"+s1+""+ s2++);
 
             if(a%9==0){
-                Thread.sleep(10);
+                Thread.sleep(1000);
                 System.out.println(h1 + "" + h2+":"+m1+""+m2+":"+s1+""+ s2++);
                 s2=0;
                 s1++;
@@ -29,7 +27,7 @@ public class Sql {
                 m2++;
             }
 
-            if(s1==6 && a%9==0 && m2==9 && h2<9){
+            if(s1==6 && a%9==0 && m2==9 && h2<=9){
                 m2=0;
                 m1++;
                 s1=0;
@@ -37,14 +35,15 @@ public class Sql {
 
             }
 
+
             if(m1==6){
 
                 m1=0;
                 h2++;
             }
 
-            if(m1==0 && m2==0 && s1==0 && s2==0 && h2==9){
 
+            if(m1==0 && m2==0 && s1==0 && s2==0 && h2==10){
                 h2=0;
                 h1++;
 
